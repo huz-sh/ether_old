d098462b5af87a381adea74f0a08f740f05666e0
huz-sh <huzsh@tuta.io>
2020-04-01 18:44:58 +0530

    argument error handling

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 mkdir -p bin
 gcc -Wall -g -O0 -o bin/ether ether/ether.c
-bin/ether    # for now we run the compiler
+bin/ether ether/ether.c    # for now we run the compiler here
--- ether/ether.c
+++ ether/ether.c
@@ -1,5 +1,33 @@
 #include <stdio.h>
+#include <stdarg.h>
+
+#define ether_error(x, ...) _ether_error(__FILE__, __LINE__, \
+										 x, ##__VA_ARGS__)
+
+static
+void _ether_error(const char* file, int line, const char* fmt, ...) {
+	printf("ether: ");
+
+#ifdef _DEBUG
+	printf("(%s:%d) ", file, line);
+#endif
+
+	va_list ap;
+	va_start(ap, fmt);
+	vprintf(fmt, ap);
+	va_end(ap);
+	printf("\n");
+}
+
+typedef struct {
+	
+} lexer;
 
 int main(int argc, char** argv) {
-	puts("Hello, ether compiler!");
+	if (argc < 2) {
+		ether_error("no input files supplied.");			
+	}
+	
+	//lexer l = { };
+	//lexer_init(&l);
 }
